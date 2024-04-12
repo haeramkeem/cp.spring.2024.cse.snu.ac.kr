f9fde06c8e0e8dd085538c98d39bc7f2be3a2b1d
haeramkeem <ewqdsacxz2345@gmail.com>
2024-04-12 10:43:32 +0900

    fix: remove obj pointer if not neccesary

--- ans.cpp
+++ ans.cpp
@@ -10,24 +10,24 @@ using namespace std;
 
 class Linter {
     protected:
-        queue<char> *q;
+        queue<char> q;
     public:
         Linter(string str_in) {
-            this->q = new queue<char>();
             for (string::iterator it = str_in.begin(); it < str_in.end(); it++) {
-                q->push(*it);
+                q.push(*it);
             }
         }
 
         void print() {
-            int q_size = q->size();
+            int q_size = q.size();
             for (int i = 0; i < q_size; i++) {
-                cout << q->front();
-                q->push(q->front());
-                q->pop();
+                cout << q.front();
+                q.push(q.front());
+                q.pop();
             }
         }
 
+        virtual ~Linter() {}
         virtual void lint() = 0;
 };
 
@@ -37,31 +37,31 @@ class LinterLv1: public Linter {
 
         void lint() {
             stack<char> stk;
-            int q_size = q->size();
+            int q_size = q.size();
 
             for (int i = 0; i < q_size; i++) {
-                char cur = q->front();
+                char cur = q.front();
                 switch (cur) {
                     case '{':
                         stk.push(cur);
-                        q->push(cur);
-                        q->push(NEWLINE);
+                        q.push(cur);
+                        q.push(NEWLINE);
                         break;
                     case ';':
-                        q->push(cur);
-                        q->push(NEWLINE);
+                        q.push(cur);
+                        q.push(NEWLINE);
                         break;
                     case '}':
                         stk.pop();
-                        q->push(cur);
+                        q.push(cur);
                         break;
                     default:
-                        if (q->back() == NEWLINE)
+                        if (q.back() == NEWLINE)
                             for (int j = 0; j < stk.size(); j++)
-                                q->push(TAB);
-                        q->push(cur);
+                                q.push(TAB);
+                        q.push(cur);
                 }
-                q->pop();
+                q.pop();
             }
         }
 };
@@ -70,9 +70,10 @@ int main() {
     string in;
     getline(cin, in);
 
-    Linter *lv1 = new LinterLv1(in);
+    Linter* lv1 = new LinterLv1(in);
     lv1->lint();
     lv1->print();
+    delete lv1;
 
     return 0;
 }
